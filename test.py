55b3c3f4d208cc792124837cf040926f4dcfc83d
Valentin <valentin.rublack@gmail.com>
2019-02-16 14:50:12 +0100

    Changed eval score so that the average of all the result positions with the same score is taken

--- test.py
+++ test.py
@@ -43,6 +43,18 @@ def filename_to_index(system_paths, testcase_paths):
 
     return indexes
 
+def range_find(sorted_list, val):
+    start, end = None, None
+    for i in range(len(sorted_list)):
+        if sorted_list[i] == val:
+            if start is None:
+                start = i
+                end = i
+            else:
+                end += 1
+
+    return start, end
+
 
 
 def unit_test(rootDir, testcase_path, out_file):
@@ -57,6 +69,7 @@ def unit_test(rootDir, testcase_path, out_file):
     testcases = read_testcases(testcase_path, file_dirs)
 
     testcase_pos = []
+    testcase_pos_scored = []
     all_results = []
 
     for t in testcases:
@@ -65,21 +78,24 @@ def unit_test(rootDir, testcase_path, out_file):
         # where is the testcase target?
         for i, r in enumerate(results):
             if r[1] == t[1]:
+                # use the average of all positions with the the same score
+                start, end = range_find(list(zip(*results))[0], r[0])
+                testcase_pos_scored.append((end + start) / 2)
                 testcase_pos.append(i)
                 break
         else:
             # not found
             testcase_pos.append(-1)
+            testcase_pos_scored.append(-1)
 
     # if not found, partial error 100
-    testcase_errors = list(map(lambda x: 100 if x == -1 else x, testcase_pos))
-    error = sum(testcase_errors)
+    error = sum(testcase_pos_scored)
 
     with open(out_file, 'w') as out:
-        out.write('Total error: {} (sum of target pos)\nTestcase errors: {}\n'.format(error, ' '.join(map(str, testcase_errors))))
+        out.write('Total error: {} (sum of target pos)\nTestcase errors: {}\n'.format(error, ' '.join(map(str, testcase_pos_scored))))
 
         for i, t in enumerate(testcases):
-            out.write('\n\n\n\nTESTCASE {}\nError: {}\nQuery: {}\n{}\n'.format(i + 1, testcase_errors[i], t[0], '-'*50))
+            out.write('\n\n\n\nTESTCASE {}\nError: {}\nQuery: {}\n{}\n'.format(i + 1, testcase_pos_scored[i], t[0], '-'*50))
             for j, res in enumerate(all_results[i]):
                 if testcase_pos[i] == j:
                     out.write('(target) ')
