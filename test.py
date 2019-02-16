ecd94ade647fe7c852208f6944986fd5152fc449
Valentin <valentin.rublack@gmail.com>
2019-02-16 14:30:40 +0100

    Fixed umlaut unicode problem that causes some testcase targets to not be matched

--- test.py
+++ test.py
@@ -4,30 +4,57 @@ from files import get_filtered_data
 from preprocessing import preprocess
 from search import search_query
 import csv
+import string
+import unicodedata
 
 
-def read_testcases(path):
+def read_testcases(path, system_paths):
     with open(path, newline='') as csvfile:
         r = csv.reader(csvfile)
-        return list(r)
-
+        testcases = list(r)
+        fname_indexes = filename_to_index(system_paths, list(zip(*testcases))[1])
+        processed_testcases = []
+        for i in range(len(testcases)):
+            processed_testcases.append((testcases[i][0], fname_indexes[i]))
+        return processed_testcases
 
 def path_equal(system_path, testcase_path):
     # remove double filename such as .txt.txt
     system_path = system_path[:system_path.rfind('.')]
 
+    # remove umlaut problem
+    printable = set(string.printable)
+    system_path = unicodedata.normalize('NFC', system_path)
+    testcase_path = unicodedata.normalize('NFC', testcase_path)
+
     return system_path.endswith(testcase_path)
+
+def filename_to_index(system_paths, testcase_paths):
+    # match every testcase path
+    indexes = []
+    for t in testcase_paths:
+        found = False
+        for i, s in enumerate(system_paths):
+            if path_equal(s, t):
+                assert not found
+                found = True
+                indexes.append(i)
+        assert found
+
+    return indexes
+
 
 
 def unit_test(rootDir, testcase_path, out_file):
     ''''''
-    testcases = read_testcases(testcase_path)
 
     (file_data, file_dirs) = get_filtered_data(rootDir)
     tokens_filtered = preprocess(file_data)
     token2files, filentoken2occ, token2occ = metrics.get_counts(tokens_filtered)
     filentoken2tfidf = metrics.get_tfidf(tokens_filtered, token2files, filentoken2occ)
     print('Loading done.')
+
+    testcases = read_testcases(testcase_path, file_dirs)
 
     testcase_pos = []
     all_results = []
@@ -37,7 +64,7 @@ def unit_test(rootDir, testcase_path, out_file):
         all_results.append(results)
         # where is the testcase target?
         for i, r in enumerate(results):
-            if path_equal(file_dirs[r[1]], t[1]):
+            if r[1] == t[1]:
                 testcase_pos.append(i)
                 break
         else:
